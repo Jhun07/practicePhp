66a9aa0720a14b02d335cdb222c9f0a677a913e1
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-02-05 18:13:04 +0800

    this is for web log in form

--- index.php
+++ index.php
@@ -8,10 +8,48 @@
     <title>php</title>
 </head>
 <body>
-    <?php 
-        echo "<hr><br>this is jeric";
-    echo "<hr><br>this is aldrin";
+
+<form method = "index.php" action = "post">
+        Enter your name <input type="name" placeholder ="Enter your name..">
+
+        Enter your password
+        <input type="password" placeholder ="Enter password..">
+
+        <input type="submit" value="submit">
+    </form>
+
+
+
+   <?php 
+    $name = $_POST["name"];
+    echo "<br>";
+    $spass = $_POST["password"]; 
     
-    ?>
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "users";
+    
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    }
+    
+    $sql = "INSERT INTO user(user_name,user_password)
+    VALUES ('$name', '$spass')";
+    
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    
+    $conn->close();
+  ?> 
+
+
 </body>
 </html>
